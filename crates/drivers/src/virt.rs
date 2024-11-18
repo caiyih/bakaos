b60d30a6698a26c1d32a86ace4f380edad9bb783
The most intelligent Cirno <9@caiyi1.me>
2024-11-18 22:32:10 +0800

    feat: integrate allocation and address modules into VirtHal for DMA management

--- crates/drivers/src/virt.rs
+++ crates/drivers/src/virt.rs
@@ -1,3 +1,5 @@
+use address::{IAddressBase, IPageNum, PhysicalAddress, PhysicalPageNum};
+use core::{mem::forget, ptr::NonNull};
 use virtio_drivers::{device::blk::VirtIOBlk, transport::mmio::MmioTransport};
 
 use crate::IDiskDevice;
@@ -53,5 +55,78 @@ where
 
     fn move_cursor(&mut self, amount: usize) {
         self.set_position(self.get_position() + amount)
+    }
+}
+
+pub struct VirtHal;
+
+unsafe impl virtio_drivers::Hal for VirtHal {
+    fn dma_alloc(
+        pages: usize,
+        _direction: virtio_drivers::BufferDirection,
+    ) -> (virtio_drivers::PhysAddr, core::ptr::NonNull<u8>) {
+        let frame_range = allocation::alloc_contiguous(pages)
+            .expect("Failed to allocate contiguous frames for Virt DMA");
+
+        let paddr = frame_range.to_range().start;
+        let vaddr =
+            unsafe { NonNull::new_unchecked((paddr | constants::VIRT_ADDR_OFFSET) as *mut u8) };
+
+        forget(frame_range); // Prevent deallocation
+
+        (paddr, vaddr)
+    }
+
+    unsafe fn dma_dealloc(
+        paddr: virtio_drivers::PhysAddr,
+        vaddr: core::ptr::NonNull<u8>,
+        pages: usize,
+    ) -> i32 {
+        // ensure paddr is a physical address
+        debug_assert!(paddr & constants::VIRT_ADDR_OFFSET == 0);
+        // ensure paddr is properly mapped to vaddr
+        debug_assert!(paddr & constants::VIRT_ADDR_OFFSET == vaddr.as_ptr() as usize);
+
+        let ppn = PhysicalPageNum::from_addr_floor(PhysicalAddress::from_usize(paddr));
+        for i in 0..pages {
+            allocation::dealloc_frame_unchecked(ppn + i);
+        }
+        0
+    }
+
+    unsafe fn mmio_phys_to_virt(
+        paddr: virtio_drivers::PhysAddr,
+        _size: usize,
+    ) -> core::ptr::NonNull<u8> {
+        // Refer to kernel virtual memory layout for more details
+        NonNull::new_unchecked((paddr | constants::VIRT_ADDR_OFFSET) as *mut u8)
+    }
+
+    unsafe fn share(
+        buffer: core::ptr::NonNull<[u8]>,
+        _direction: virtio_drivers::BufferDirection,
+    ) -> virtio_drivers::PhysAddr {
+        let address = buffer.as_ptr() as *mut u8 as usize;
+
+        // Ensure that the address is a virtual address
+        debug_assert!(address & constants::VIRT_ADDR_OFFSET == constants::VIRT_ADDR_OFFSET);
+
+        // We can even return the virtual as the whole kernel space is mapped to the higher half
+        // And we don't have to worry about the physical
+        (address & constants::PHYS_ADDR_MASK) as virtio_drivers::PhysAddr
+    }
+
+    unsafe fn unshare(
+        _paddr: virtio_drivers::PhysAddr,
+        _buffer: core::ptr::NonNull<[u8]>,
+        _direction: virtio_drivers::BufferDirection,
+    ) {
+        #[cfg(debug_assertions)]
+        {
+            // Ensure that the address is a virtual address
+            debug_assert!(_paddr & constants::VIRT_ADDR_OFFSET == constants::VIRT_ADDR_OFFSET);
+        }
+
+        // Do nothing
     }
 }
