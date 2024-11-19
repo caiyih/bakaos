e4869de15821668cc703568fb2d80abfdf0073bc
The most intelligent Cirno <9@caiyi1.me>
2024-11-19 18:19:11 +0800

    feat: implement setup for FAT32 root filesystem in kernel

--- crates/filesystem/src/lib.rs
+++ crates/filesystem/src/lib.rs
@@ -8,3 +8,19 @@ extern crate alloc;
 mod fatfs_impl;
 
 pub use fatfs_impl::*;
+use filesystem_abstractions::FileSystem;
+use log::debug;
+
+pub type RootFileSystemType = fatfs_impl::Fat32FileSystem;
+
+static mut ROOT_FILESYSTEM: Option<RootFileSystemType> = None;
+
+pub fn setup_root_filesystem(fs: RootFileSystemType) {
+    debug!("Initializing filesystem: {}", fs.name());
+
+    unsafe { ROOT_FILESYSTEM = Some(fs) };
+}
+
+pub fn root_filesystem() -> &'static RootFileSystemType {
+    unsafe { ROOT_FILESYSTEM.as_ref().expect("Root filesystem not initialized") }
+}
