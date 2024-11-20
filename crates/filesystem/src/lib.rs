110edb03c71b3b051510de78cac4faebf4c8d1c3
The most intelligent Cirno <9@caiyi1.me>
2024-11-20 18:41:19 +0800

    feat: implement inode caching mechanism with InodeCacheAccessor

--- crates/filesystem/src/lib.rs
+++ crates/filesystem/src/lib.rs
@@ -7,8 +7,9 @@ extern crate alloc;
 
 mod fatfs_impl;
 
+use alloc::{sync::Arc, vec::Vec};
 pub use fatfs_impl::*;
-use filesystem_abstractions::IFileSystem;
+use filesystem_abstractions::{IFileSystem, IInode};
 use log::debug;
 
 pub type RootFileSystemType = fatfs_impl::Fat32FileSystem;
@@ -26,5 +27,54 @@ pub fn root_filesystem() -> &'static dyn IFileSystem {
         ROOT_FILESYSTEM
             .as_ref()
             .expect("Root filesystem not initialized")
+    }
+}
+
+static mut INODE_CACHE: Vec<Arc<dyn IInode>> = Vec::new();
+
+pub trait ICacheableInode: IInode {
+    fn cache_as_arc_accessor(self: &Arc<Self>) -> Arc<InodeCacheAccessor>;
+    fn cache_as_accessor(self: &Arc<Self>) -> InodeCacheAccessor;
+}
+
+impl ICacheableInode for dyn IInode {
+    fn cache_as_accessor(self: &Arc<Self>) -> InodeCacheAccessor {
+        InodeCacheAccessor::new(self.clone())
+    }
+
+    fn cache_as_arc_accessor(self: &Arc<Self>) -> Arc<InodeCacheAccessor> {
+        Arc::new(self.cache_as_accessor())
+    }
+}
+
+pub struct InodeCacheAccessor {
+    inode_id: usize,
+}
+
+impl InodeCacheAccessor {
+    pub fn new(inode: Arc<dyn IInode>) -> Self {
+        let inode_id = unsafe {
+            let inode_id = INODE_CACHE.len();
+            INODE_CACHE.push(inode);
+            inode_id
+        };
+
+        InodeCacheAccessor { inode_id }
+    }
+
+    pub fn access(&self) -> Arc<dyn IInode> {
+        unsafe { INODE_CACHE[self.inode_id].clone() }
+    }
+
+    pub fn inode_id(&self) -> usize {
+        self.inode_id
+    }
+}
+
+impl Drop for InodeCacheAccessor {
+    fn drop(&mut self) {
+        unsafe {
+            INODE_CACHE.swap_remove(self.inode_id);
+        }
     }
 }
