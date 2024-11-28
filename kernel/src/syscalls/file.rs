452655ab00290a1cc544125eb8cf2d6d6109dbf7
The most intelligent Cirno <9@caiyi1.me>
2024-11-28 12:34:45 +0800

    feat: update WriteSyscall to utilize file descriptor for writing and add error handling

--- kernel/src/syscalls/file.rs
+++ kernel/src/syscalls/file.rs
@@ -1,16 +1,26 @@
 use paging::IWithPageGuardBuilder;
 
 use super::{ISyncSyscallHandler, SyscallContext, SyscallResult};
-use crate::legacy_print;
 
 pub struct WriteSyscall;
 
 impl ISyncSyscallHandler for WriteSyscall {
-    // FIXME: should use the file descriptor to write to the correct file
     fn handle(&self, ctx: &mut SyscallContext<'_>) -> SyscallResult {
-        let _fd = ctx.arg0::<i32>();
+        let fd = ctx.arg0::<usize>();
         let p_buf = ctx.arg1::<*const u8>();
         let len = ctx.arg2::<usize>();
+
+        let fd = ctx.tcb.fd_table.lock().get(fd);
+
+        if fd.is_none() {
+            return Err(-1);
+        }
+
+        let fd = fd.unwrap();
+
+        if !fd.can_write() {
+            return Err(-1);
+        }
 
         let buf = unsafe { core::slice::from_raw_parts(p_buf, len) };
 
@@ -21,13 +31,7 @@ impl ISyncSyscallHandler for WriteSyscall {
             .mustbe_user()
             .with_read()
         {
-            Some(guard) => {
-                for c in guard.iter() {
-                    legacy_print!("{}", *c as char);
-                }
-
-                Ok(guard.len() as isize)
-            }
+            Some(guard) => Ok(fd.file_handle().access().write(&guard) as isize),
             None => Err(-1),
         }
     }
