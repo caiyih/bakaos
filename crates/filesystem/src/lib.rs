80772726e420704744f023b2cd155ad23897bbbc
The most intelligent Cirno <9@caiyi1.me>
2024-11-28 23:15:52 +0800

    feat: implement DummyFileSystem and DummyInode for testing purposes

--- crates/filesystem/src/lib.rs
+++ crates/filesystem/src/lib.rs
@@ -10,11 +10,33 @@ mod fatfs_impl;
 use alloc::sync::Arc;
 
 pub use fatfs_impl::*;
-use filesystem_abstractions::{mount_at, IFileSystem};
+use filesystem_abstractions::{mount_at, IFileSystem, IInode};
 
 pub type RootFileSystemType = fatfs_impl::Fat32FileSystem;
 
 pub fn setup_root_filesystem(fs: RootFileSystemType) {
     let root: Arc<dyn IFileSystem> = Arc::new(fs);
     mount_at(root, "/");
+}
+
+pub struct DummyInode;
+
+impl IInode for DummyInode {}
+
+pub struct DummyFileSystem;
+
+impl Default for DummyFileSystem {
+    fn default() -> Self {
+        DummyFileSystem
+    }
+}
+
+impl IFileSystem for DummyFileSystem {
+    fn root_dir(&'static self) -> Arc<dyn IInode> {
+        Arc::new(DummyInode)
+    }
+
+    fn name(&self) -> &str {
+        "DummyFileSystem"
+    }
 }
