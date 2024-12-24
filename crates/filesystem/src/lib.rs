6f9e47b46e1cd7506b8a55d5d828ff626847a8cf
The most intelligent Cirno <9@caiyi1.me>
2024-12-24 23:56:26 +0800

    feat: add lwext4 filesystem support with commentted out code
    lwext4 makes compiling extremely painful, so im trying my best not using it

--- crates/filesystem/src/lib.rs
+++ crates/filesystem/src/lib.rs
@@ -7,12 +7,14 @@ extern crate alloc;
 
 mod ext4_impl;
 mod fatfs_impl;
+// mod lwext4rs_impl;
 
 use alloc::sync::Arc;
 
 pub use ext4_impl::Ext4FileSystem;
 pub use fatfs_impl::Fat32FileSystem;
 use filesystem_abstractions::{mount_at, IFileSystem, IInode};
+// pub use lwext4rs_impl::Lwext4FileSystem;
 
 pub fn setup_root_filesystem(fs: impl IFileSystem + 'static) {
     let root: Arc<dyn IFileSystem> = Arc::new(fs);
