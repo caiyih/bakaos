43aeed4b042bc8e6b0aa963d382b990b94a23db9
The most intelligent Cirno <9@caiyi1.me>
2025-01-20 12:31:32 +0800

    feat: implement `sys_sendfile_async` syscall

--- kernel/src/syscalls/file_async.rs
+++ kernel/src/syscalls/file_async.rs
@@ -1,3 +1,7 @@
+use core::mem::MaybeUninit;
+
+use alloc::sync::Arc;
+use filesystem_abstractions::FileMetadata;
 use paging::{page_table::IOptionalPageGuardBuilderExtension, IWithPageGuardBuilder};
 use threading::yield_now;
 
@@ -134,4 +138,82 @@ async_syscall!(sys_writev_async, ctx, {
         }
         None => Err(-1isize),
     }
+});
+
+async_syscall!(sys_sendfile_async, ctx, {
+    let fd_table = ctx.tcb.fd_table.lock();
+
+    let out_fd = fd_table.get(ctx.arg0::<usize>()).ok_or(-1isize)?;
+    let in_fd = fd_table.get(ctx.arg1::<usize>()).ok_or(-1isize)?;
+
+    if !out_fd.can_write() || !in_fd.can_read() {
+        return Err(-1isize);
+    }
+
+    let in_file = in_fd.access();
+    let in_meta = in_file.metadata();
+
+    let poffset = ctx.arg2::<*const usize>();
+    let size = ctx.arg3::<usize>();
+
+    fn get_avaliable_size(
+        file_meta: &Option<Arc<FileMetadata>>,
+        size: usize,
+    ) -> (Option<usize>, bool) {
+        if let Some(file_meta) = file_meta {
+            return match file_meta.inode().metadata() {
+                Ok(inode_meta) => (
+                    Some(usize::min(inode_meta.size - file_meta.offset(), size)),
+                    true,
+                ),
+                Err(_) => (None, true),
+            };
+        }
+
+        (None, false)
+    }
+
+    let (mut file_size, seekable) = get_avaliable_size(&in_meta, size);
+
+    let out_file = out_fd.access();
+
+    if seekable && !poffset.is_null() {
+        let offset: usize = *ctx
+            .tcb
+            .borrow_page_table()
+            .guard_ptr(poffset)
+            .mustbe_user()
+            .with_read()
+            .ok_or(-1isize)?;
+
+        in_meta.as_ref().unwrap().set_offset(offset);
+    }
+
+    let mut bytes_written = 0;
+    while file_size == None || file_size != Some(0) {
+        let buf: MaybeUninit<[u8; 512]> = MaybeUninit::uninit();
+        let mut buf: [u8; 512] = unsafe { core::mem::transmute::<_, _>(buf) };
+
+        while !in_file.read_avaliable() {
+            yield_now().await;
+        }
+
+        let bytes_read = in_file.read(&mut buf[..usize::min(512, file_size.unwrap_or(512))]);
+
+        while !out_file.write_avaliable() {
+            yield_now().await;
+        }
+
+        bytes_written += out_file.write(&buf[..bytes_read]);
+
+        if let Some(old_size) = file_size {
+            file_size = Some(old_size - bytes_read);
+        }
+
+        if seekable {
+            let in_meta = in_meta.as_ref().unwrap();
+            in_meta.set_offset(in_meta.offset() + bytes_read);
+        }
+    }
+    Ok(bytes_written as isize)
 });
