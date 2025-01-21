a71c42447b862fbb7b625b853586a7b363355173
The most intelligent Cirno <9@caiyi1.me>
2025-01-21 23:31:21 +0800

    feat: add errno module for enhanced error return value represent in syscalls

--- crates/constants/src/lib.rs
+++ crates/constants/src/lib.rs
@@ -3,6 +3,9 @@
 mod build_info;
 pub use build_info::*;
 
+mod errno;
+pub use errno::{ErrNo, SyscallError};
+
 pub const PAGE_SIZE: usize = 4096;
 pub const KERNEL_HEAP_SIZE: usize = 0x0080_0000;
 pub const VIRT_ADDR_OFFSET: usize = 0xffff_ffc0_0000_0000;
