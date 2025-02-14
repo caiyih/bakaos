41536f8d09f768611051f6a4f821ba67e2f4fa5b
Caiyi Shyu <cai1hsu@outlook.com>
2025-02-14 18:17:07 +0800

    fix: compare before requeue for `FUTEX_OP_CMP_REQUEUE`

--- kernel/src/syscalls/futex_async.rs
+++ kernel/src/syscalls/futex_async.rs
@@ -44,34 +44,58 @@ async_syscall!(sys_futex_async, ctx, {
     const FUTEX_OP_REQUEUE: i32 = 3;
     const FUTEX_OP_CMP_REQUEUE: i32 = 4;
 
-    match ctx.arg1::<i32>() /* futex_op */ {
+    let futex_op = ctx.arg1::<i32>();
+
+    match futex_op {
         FUTEX_OP_WAIT => {
             let val = ctx.arg2::<u32>();
             let uaddr = ctx.arg0::<VirtualAddress>();
 
             // validate uaddr
-            ctx.borrow_page_table().guard_ptr(unsafe { uaddr.as_ptr::<u32>() }).mustbe_user().with_read().ok_or(ErrNo::BadAddress)?;
+            ctx.borrow_page_table()
+                .guard_ptr(unsafe { uaddr.as_ptr::<u32>() })
+                .mustbe_user()
+                .with_read()
+                .ok_or(ErrNo::BadAddress)?;
 
             let p_timeout = ctx.arg3::<*const TimeSpec>();
-            let timeout = ctx.borrow_page_table().guard_ptr(p_timeout).mustbe_user().with_read().map(|g| *g);
+            let timeout = ctx
+                .borrow_page_table()
+                .guard_ptr(p_timeout)
+                .mustbe_user()
+                .with_read()
+                .map(|g| *g);
 
-            if unsafe { atomic_load_acquire(uaddr.as_ptr::<u32>()) }  != val {
+            if unsafe { atomic_load_acquire(uaddr.as_ptr::<u32>()) } != val {
                 return SyscallError::ResourceTemporarilyUnavailable;
             }
 
             // Split to make state machine smaller
             futex_wait(ctx, uaddr, val, timeout).await;
-        },
-        FUTEX_OP_WAKE => return Ok(ctx.pcb.lock().futex_queue.wake(
+        }
+        FUTEX_OP_WAKE => {
+            return Ok(ctx.pcb.lock().futex_queue.wake(
                 ctx.arg0::<VirtualAddress>(), // uaddr
-                ctx.arg2::<usize>(), // nval
-            ) as isize),
-        FUTEX_OP_REQUEUE | FUTEX_OP_CMP_REQUEUE => return Ok(ctx.pcb.lock().futex_queue.requeue(
+                ctx.arg2::<usize>(),          // nval
+            ) as isize);
+        }
+        FUTEX_OP_REQUEUE | FUTEX_OP_CMP_REQUEUE => {
+            if futex_op == FUTEX_OP_CMP_REQUEUE {
+                let val = ctx.arg2::<u32>();
+                let uaddr = ctx.arg0::<VirtualAddress>();
+
+                if unsafe { atomic_load_acquire(uaddr.as_ptr::<u32>()) } != val {
+                    return SyscallError::ResourceTemporarilyUnavailable;
+                }
+            }
+
+            return Ok(ctx.pcb.lock().futex_queue.requeue(
                 ctx.arg0::<VirtualAddress>(), // prev_addr
                 ctx.arg4::<VirtualAddress>(), // new_addr
-                ctx.arg2::<usize>(), // n_wake
-                ctx.arg3::<usize>(), // n_requeue
-            ) as isize),
+                ctx.arg2::<usize>(),          // n_wake
+                ctx.arg3::<usize>(),          // n_requeue
+            ) as isize);
+        }
         _ => (),
     }
 
