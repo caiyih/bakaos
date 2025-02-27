8d0d56d72c9ff5caa6922eea26dbda04d7a89387
Caiyi Shyu <cai1hsu@outlook.com>
2025-02-27 22:19:41 +0800

    feat: add time offset initialization and adjust current time functions

--- kernel/src/timing.rs
+++ kernel/src/timing.rs
@@ -12,18 +12,28 @@ pub trait ITimer {
     fn elapsed(&self) -> TimeSpan;
 }
 
+static mut TIME_OFFSEST: TimeSpec = TimeSpec {
+    tv_sec: 0,
+    tv_nsec: 0,
+};
+
+pub fn initialize() {
+    unsafe { TIME_OFFSEST = kernel::get().machine().get_rtc_offset() };
+}
+
 pub fn current_timespec() -> TimeSpec {
     let machine = kernel::get().machine();
     let ticks = machine.get_board_tick() as i64;
     let freq = machine.clock_freq();
-    TimeSpec::from_ticks(ticks, freq)
+    TimeSpec::from_ticks(ticks, freq) + unsafe { TIME_OFFSEST }
 }
 
+#[allow(static_mut_refs)]
 pub fn current_timeval() -> TimeVal {
     let machine = kernel::get().machine();
     let ticks = machine.get_board_tick() as i64;
     let freq = machine.clock_freq();
-    TimeVal::from_ticks(ticks, freq)
+    TimeVal::from_ticks(ticks, freq) + unsafe { TIME_OFFSEST.to_timeval() }
 }
 
 impl ITimer for UserTaskTimer {
