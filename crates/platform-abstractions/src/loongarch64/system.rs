f4ae7f6a43c57821e64b6f741a034b700ee39d60
Caiyi Shyu <cai1hsu@outlook.com>
2025-03-02 21:27:54 +0800

    fix: update system module for loongarch64 to implement shutdown and bootloader info printing

--- crates/platform-abstractions/src/loongarch64/system.rs
+++ crates/platform-abstractions/src/loongarch64/system.rs
@@ -1,10 +1,12 @@
-use platform_specific::console_writestr;
+use platform_specific::{legacy_println, phys_to_virt};
+
+const HALT_ADDR: *mut u8 = phys_to_virt(0x100E001C) as *mut u8;
 
 pub fn machine_shutdown(_failure: bool) -> ! {
-    // TODO: this is a stub
+    unsafe { HALT_ADDR.write_volatile(0x34) };
     loop {}
 }
 
 pub fn print_bootloader_info() {
-    console_writestr(b"Platform: loongarch64");
+    legacy_println!("Platform: loongarch64");
 }
