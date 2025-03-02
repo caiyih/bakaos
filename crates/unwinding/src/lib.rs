b611ec1f1b2baa92bc784476b02e616052b057d0
Caiyi Shyu <cai1hsu@outlook.com>
2025-03-02 21:27:38 +0800

    feat: add support for loongarch64 architecture in stack unwinding

--- crates/unwinding/src/lib.rs
+++ crates/unwinding/src/lib.rs
@@ -20,11 +20,18 @@ impl StackTrace {
     #[inline(always)]
     #[allow(unused)]
     pub fn begin_unwind(skip_frames: usize) -> StackTrace {
-        #[cfg(not(any(target_arch = "riscv64")))]
+        #[cfg(not(any(target_arch = "riscv64", target_arch = "loongarch64")))]
         panic!("Unsupported architecture");
 
+        #[cfg(target_arch = "loongarch64")]
+        {
+            Self::loongarch64_begin_unwind(skip_frames)
+        }
+
         #[cfg(target_arch = "riscv64")]
-        Self::riscv64_begin_unwind(skip_frames)
+        {
+            Self::riscv64_begin_unwind(skip_frames)
+        }
     }
 
     pub fn stack_frames(&self) -> &[StackFrame] {
@@ -44,8 +51,8 @@ impl StackFrame {
     }
 }
 
+#[cfg(target_arch = "riscv64")]
 impl StackTrace {
-    #[cfg(target_arch = "riscv64")]
     #[inline(always)]
     fn riscv64_begin_unwind(mut skip_frames: usize) -> StackTrace {
         extern "C" {
@@ -67,6 +74,35 @@ impl StackTrace {
 
             fp = unsafe { *(fp as *const usize).offset(-2) };
             ra = unsafe { *(fp as *const usize).offset(-1) };
+        }
+
+        StackTrace { frames }
+    }
+}
+
+#[cfg(target_arch = "loongarch64")]
+impl StackTrace {
+    #[inline(always)]
+    fn loongarch64_begin_unwind(mut skip_frames: usize) -> StackTrace {
+        extern "C" {
+            fn stext();
+            fn etext();
+        }
+
+        let mut ra = platform_specific::ra();
+        let mut fp = platform_specific::fp();
+        let mut frames = Vec::new();
+
+        while ra >= stext as usize && ra <= etext as usize && fp >= stext as usize && fp != 0 {
+            if skip_frames == 0 {
+                let pc = ra - 4; // all instructions on loongarch64 are 32-bit
+                frames.push(StackFrame { pc: Ok(pc), fp })
+            } else {
+                skip_frames -= 1;
+            }
+
+            ra = unsafe { *(fp as *const usize).offset(-1) };
+            fp = unsafe { *(fp as *const usize).offset(-2) };
         }
 
         StackTrace { frames }
