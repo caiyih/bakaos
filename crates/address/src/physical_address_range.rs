acccd3392b396256d2f80a70e48ff793e6968bae
Caiyi Shyu <cai1hsu@outlook.com>
2025-03-02 14:44:54 +0800

    refactor: remove unused to_high_virtual method from PhysicalAddressRange implementation

--- crates/address/src/physical_address_range.rs
+++ crates/address/src/physical_address_range.rs
@@ -3,12 +3,3 @@ use crate::*;
 pub type PhysicalAddressRange = AddressRange<PhysicalAddress>;
 
 impl_range_display!(PhysicalAddressRange);
-
-impl PhysicalAddressRange {
-    pub fn to_high_virtual(&self) -> VirtualAddressRange {
-        VirtualAddressRange::from_start_end(
-            self.start().to_high_virtual(),
-            self.end().to_high_virtual(),
-        )
-    }
-}
