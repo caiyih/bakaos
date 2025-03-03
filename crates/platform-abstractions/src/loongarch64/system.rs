d8665a1541688c64139ad2ef65e0a4017232b160
Caiyi Shyu <cai1hsu@outlook.com>
2025-03-03 13:01:49 +0800

    fix: update HALT_ADDR calculation for loongarch64 architecture

--- crates/platform-abstractions/src/loongarch64/system.rs
+++ crates/platform-abstractions/src/loongarch64/system.rs
@@ -1,7 +1,8 @@
-use platform_specific::{legacy_println, phys_to_virt};
+use platform_specific::legacy_println;
 
-const HALT_ADDR: *mut u8 = phys_to_virt(0x100E001C) as *mut u8;
+const HALT_ADDR: *mut u8 = (0x8000_0000_0000_0000usize | 0x100E001C) as *mut u8;
 
+#[no_mangle]
 pub fn machine_shutdown(_failure: bool) -> ! {
     unsafe { HALT_ADDR.write_volatile(0x34) };
     loop {}
