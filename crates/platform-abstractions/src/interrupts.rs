60b95c323f5540226d620e75e2d27f2db9ae7014
Caiyi Shyu <cai1hsu@outlook.com>
2025-03-04 16:09:03 +0800

    feat: implement custom Debug trait for UserInterrupt enum to enhance debugging output

--- crates/platform-abstractions/src/interrupts.rs
+++ crates/platform-abstractions/src/interrupts.rs
@@ -1,7 +1,6 @@
 use alloc::boxed::Box;
 use core::fmt::Debug;
 
-#[derive(Debug)]
 pub enum UserInterrupt {
     Unknown(Box<dyn Debug + Send + Sync>),
     Breakpoint,
@@ -16,4 +15,37 @@ pub enum UserInterrupt {
     InstructionMisaligned(usize),
     IllegalInstruction(usize),
     AccessFault(usize),
+}
+
+impl Debug for UserInterrupt {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            Self::Unknown(payload) => payload.fmt(f),
+            Self::Breakpoint => write!(f, "Breakpoint"),
+            Self::Syscall => write!(f, "Syscall"),
+            Self::Timer => write!(f, "Timer"),
+            Self::SupervisorExternal => write!(f, "SupervisorExternal"),
+            memory_interrupt => {
+                let (type_str, stval) = match memory_interrupt {
+                    UserInterrupt::StorePageFault(stval) => ("StorePageFault", stval),
+                    UserInterrupt::StoreMisaligned(stval) => ("StoreMisaligned", stval),
+                    UserInterrupt::LoadPageFault(stval) => ("LoadPageFault", stval),
+                    UserInterrupt::LoadMisaligned(stval) => ("LoadMisaligned", stval),
+                    UserInterrupt::InstructionPageFault(stval) => ("InstructionPageFault", stval),
+                    UserInterrupt::InstructionMisaligned(stval) => ("InstructionMisaligned", stval),
+                    UserInterrupt::IllegalInstruction(stval) => ("IllegalInstruction", stval),
+                    UserInterrupt::AccessFault(stval) => ("AccessFault", stval),
+                    _ => unreachable!(),
+                };
+
+                match () {
+                    #[cfg(target_pointer_width = "64")]
+                    () => write!(f, "{}({:#018x})", type_str, stval),
+
+                    #[cfg(target_pointer_width = "32")]
+                    () => write!(f, "{}({:#010x})", type_str, stval),
+                }
+            }
+        }
+    }
 }
