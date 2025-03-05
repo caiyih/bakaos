e9d765d6295e3731c8b2ad5c85fa5ce46f005e72
Caiyi Shyu <cai1hsu@outlook.com>
2025-03-05 20:30:20 +0800

    fix: correct PLATFORM_STRING value to match proper casing for LoongArch64

--- crates/platform-specific/src/loongarch64/mod.rs
+++ crates/platform-specific/src/loongarch64/mod.rs
@@ -12,7 +12,7 @@ pub use serial::*;
 
 // IMPORTANT: Must provide for every platform
 // FIXME: Figure out the correct value for this
-pub const PLATFORM_STRING: &CStr = c"Loongarch64";
+pub const PLATFORM_STRING: &CStr = c"LoongArch64";
 
 pub const PHYS_ADDR_MASK: usize = 0x0000_7FFF_FFFF_FFFF; // keep to lower half
 pub const VIRT_ADDR_OFFSET: usize = 0x9000_0000_0000_0000; // to higher half
