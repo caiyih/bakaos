b5aa6ec3550fe03498bd71a0a03999c7f2a72b93
Caiyi Shyu <cai1hsu@outlook.com>
2025-03-06 19:49:27 +0800

    feat: add FloatRegisterContext with activation and snapshot functionality for LoongArch64

--- crates/platform-specific/src/loongarch64/context.rs
+++ crates/platform-specific/src/loongarch64/context.rs
@@ -39,6 +39,59 @@ pub struct GeneralRegisterContext {
 
 #[repr(C)]
 #[derive(Debug, Clone, Copy)]
+pub struct FloatRegisterContext {
+    pub f0: f64,
+    pub f1: f64,
+    pub f2: f64,
+    pub f3: f64,
+    pub f4: f64,
+    pub f5: f64,
+    pub f6: f64,
+    pub f7: f64,
+    pub f8: f64,
+    pub f9: f64,
+    pub f10: f64,
+    pub f11: f64,
+    pub f12: f64,
+    pub f13: f64,
+    pub f14: f64,
+    pub f15: f64,
+    pub f16: f64,
+    pub f17: f64,
+    pub f18: f64,
+    pub f19: f64,
+    pub f20: f64,
+    pub f21: f64,
+    pub f22: f64,
+    pub f23: f64,
+    pub f24: f64,
+    pub f25: f64,
+    pub f26: f64,
+    pub f27: f64,
+    pub f28: f64,
+    pub f29: f64,
+    pub f30: f64,
+    pub f31: f64,
+    pub fcc: u64,
+    pub fcsr0: u32,
+    pub dirty: bool,
+    pub activated: bool,
+}
+
+impl FloatRegisterContext {
+    pub fn activate_restore(&mut self) {
+        self.restore();
+        self.activated = true;
+    }
+
+    pub fn deactivate(&mut self) {
+        self.snapshot();
+        self.activated = false;
+    }
+}
+
+#[repr(C)]
+#[derive(Debug, Clone, Copy)]
 pub struct TaskTrapContext {
     /// general registers.
     pub regs: GeneralRegisterContext, // 0 - 31
@@ -50,6 +103,7 @@ pub struct TaskTrapContext {
     pub badv: usize, // 34
     /// Current Mode Information
     pub crmd: usize, // 35
+    pub fregs: FloatRegisterContext,
 }
 
 impl ITaskContext for TaskTrapContext {
@@ -79,5 +133,133 @@ impl ITaskContext for TaskTrapContext {
 
     fn set_syscall_return_value(&mut self, ret: usize) {
         self.regs.a0 = ret;
+    }
+}
+
+impl FloatRegisterContext {
+    pub fn snapshot(&mut self) {
+        if self.dirty {
+            self.dirty = false;
+
+            unsafe {
+                core::arch::asm!(
+                    "dbar 0",
+                    "fst.d          $f0,    $a0,  0*8",
+                    "fst.d          $f1,    $a0,  1*8",
+                    "fst.d          $f2,    $a0,  2*8",
+                    "fst.d          $f3,    $a0,  3*8",
+                    "fst.d          $f4,    $a0,  4*8",
+                    "fst.d          $f5,    $a0,  5*8",
+                    "fst.d          $f6,    $a0,  6*8",
+                    "fst.d          $f7,    $a0,  7*8",
+                    "fst.d          $f8,    $a0,  8*8",
+                    "fst.d          $f9,    $a0,  9*8",
+                    "fst.d          $f10,   $a0, 10*8",
+                    "fst.d          $f11,   $a0, 11*8",
+                    "fst.d          $f12,   $a0, 12*8",
+                    "fst.d          $f13,   $a0, 13*8",
+                    "fst.d          $f14,   $a0, 14*8",
+                    "fst.d          $f15,   $a0, 15*8",
+                    "fst.d          $f16,   $a0, 16*8",
+                    "fst.d          $f17,   $a0, 17*8",
+                    "fst.d          $f18,   $a0, 18*8",
+                    "fst.d          $f19,   $a0, 19*8",
+                    "fst.d          $f20,   $a0, 20*8",
+                    "fst.d          $f21,   $a0, 21*8",
+                    "fst.d          $f22,   $a0, 22*8",
+                    "fst.d          $f23,   $a0, 23*8",
+                    "fst.d          $f24,   $a0, 24*8",
+                    "fst.d          $f25,   $a0, 25*8",
+                    "fst.d          $f26,   $a0, 26*8",
+                    "fst.d          $f27,   $a0, 27*8",
+                    "fst.d          $f28,   $a0, 28*8",
+                    "fst.d          $f29,   $a0, 29*8",
+                    "fst.d          $f30,   $a0, 30*8",
+                    "fst.d          $f31,   $a0, 31*8",
+                    "movcf2gr       $t0,    $fcc0",
+                    "st.b           $t0,    $a0, 32*8+0",
+                    "movcf2gr       $t0,    $fcc1",
+                    "st.b           $t0,    $a0, 32*8+1",
+                    "movcf2gr       $t0,    $fcc2",
+                    "st.b           $t0,    $a0, 32*8+2",
+                    "movcf2gr       $t0,    $fcc3",
+                    "st.b           $t0,    $a0, 32*8+3",
+                    "movcf2gr       $t0,    $fcc4",
+                    "st.b           $t0,    $a0, 32*8+4",
+                    "movcf2gr       $t0,    $fcc5",
+                    "st.b           $t0,    $a0, 32*8+5",
+                    "movcf2gr       $t0,    $fcc6",
+                    "st.b           $t0,    $a0, 32*8+6",
+                    "movcf2gr       $t0,    $fcc7",
+                    "st.b           $t0,    $a0, 32*8+7",
+                    "movfcsr2gr     $t0,    $fcsr0",
+                    "st.d           $t0,    $a0, 33*8",
+                    in("$a0") self,
+                );
+            }
+        }
+    }
+
+    pub fn restore(&mut self) {
+        if !self.activated {
+            self.activated = true;
+
+            unsafe {
+                core::arch::asm!(
+                    "movgr2cf       $fcc0,  $t0",
+                    "ld.b           $t0,    $a0, 32*8+1",
+                    "movgr2cf       $fcc1,  $t0",
+                    "ld.b           $t0,    $a0, 32*8+2",
+                    "movgr2cf       $fcc2,  $t0",
+                    "ld.b           $t0,    $a0, 32*8+3",
+                    "movgr2cf       $fcc3,  $t0",
+                    "ld.b           $t0,    $a0, 32*8+4",
+                    "movgr2cf       $fcc4,  $t0",
+                    "ld.b           $t0,    $a0, 32*8+5",
+                    "movgr2cf       $fcc5,  $t0",
+                    "ld.b           $t0,    $a0, 32*8+6",
+                    "movgr2cf       $fcc6,  $t0",
+                    "ld.b           $t0,    $a0, 32*8+7",
+                    "movgr2cf       $fcc7,  $t0",
+                    "fld.d          $f0,    $a0, 0*8",
+                    "fld.d          $f1,    $a0, 1*8",
+                    "fld.d          $f2,    $a0, 2*8",
+                    "fld.d          $f3,    $a0, 3*8",
+                    "fld.d          $f4,    $a0, 4*8",
+                    "fld.d          $f5,    $a0, 5*8",
+                    "fld.d          $f6,    $a0, 6*8",
+                    "fld.d          $f7,    $a0, 7*8",
+                    "fld.d          $f8,    $a0, 8*8",
+                    "fld.d          $f9,    $a0, 9*8",
+                    "fld.d          $f10,   $a0, 10*8",
+                    "fld.d          $f11,   $a0, 11*8",
+                    "fld.d          $f12,   $a0, 12*8",
+                    "fld.d          $f13,   $a0, 13*8",
+                    "fld.d          $f14,   $a0, 14*8",
+                    "fld.d          $f15,   $a0, 15*8",
+                    "fld.d          $f16,   $a0, 16*8",
+                    "fld.d          $f17,   $a0, 17*8",
+                    "fld.d          $f18,   $a0, 18*8",
+                    "fld.d          $f19,   $a0, 19*8",
+                    "fld.d          $f20,   $a0, 20*8",
+                    "fld.d          $f21,   $a0, 21*8",
+                    "fld.d          $f22,   $a0, 22*8",
+                    "fld.d          $f23,   $a0, 23*8",
+                    "fld.d          $f24,   $a0, 24*8",
+                    "fld.d          $f25,   $a0, 25*8",
+                    "fld.d          $f26,   $a0, 26*8",
+                    "fld.d          $f27,   $a0, 27*8",
+                    "fld.d          $f28,   $a0, 28*8",
+                    "fld.d          $f29,   $a0, 29*8",
+                    "fld.d          $f30,   $a0, 30*8",
+                    "fld.d          $f31,   $a0, 31*8",
+                    "ld.b           $t0,    $a0, 32*8+0",
+                    "ld.d           $t0,    $a0, 33*8",
+                    "movfcsr2gr     $t0,    $fcsr0",
+                    "",
+                    in("$a0") self,
+                );
+            }
+        }
     }
 }
