4b4c0b707dbcee7bf9e65e96693388488e6d2f0e
Caiyi Shyu <cai1hsu@outlook.com>
2025-03-30 22:44:36 +0800

    test(kernel): update test script to include cyclictest and refactor busybox test

--- kernel/src/test_script.sh
+++ kernel/src/test_script.sh
@@ -3,8 +3,26 @@
 	cd basic
 	echo "#### OS COMP TEST GROUP START basic-musl ####"
 	./run-all.sh
+    ./sleep
+    ./yield
 	echo "#### OS COMP TEST GROUP END basic-musl ####"
 )
 ./lua_testcode.sh
+
+echo "#### OS COMP TEST GROUP START busybox-glibc ####"
+cat ./busybox_cmd.txt | while read line
+do
+        eval "./busybox $line"
+        RTN=$?
+        if [[ $RTN -ne 0 && $line != "false" ]] ;then
+                echo ""
+                echo "testcase busybox $line fail"
+        else
+                echo "testcase busybox $line success"
+        fi
+done
+
+echo "#### OS COMP TEST GROUP END busybox-glibc ####"
+
 ./libctest_testcode.sh
-./busybox_testcode.sh
+cyclictest_testcode.sh
