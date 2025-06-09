f5a400c850f4755bf341f856a7ef1fb037ee3deb
Caiyi Shyu <cai1hsu@outlook.com>
2025-06-09 17:18:00 +0800

    build(ci): add preprocessing step for README synchronization

---
+++ .github/gitlab-prerun.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+replace_in_file() {
+    local file="$1"
+    local search="$2"
+    local replace="$3"
+
+    if [[ ! -f "$file" ]]; then
+        echo "Error: File '$file' does not exist."
+        return 1
+    fi
+
+    sed -i "s|$search|$replace|g" "$file"
+}
+
+replace_in_file "README.md" "./README.zh-cn.md" "./README.md"
+replace_in_file "README.zh-cn.md" "./README.md" "./README.en-us.md"
+
+mv README.md README.en-us.md
+mv README.zh-cn.md README.md
