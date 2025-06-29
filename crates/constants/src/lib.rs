975bd9760be4fc399c3f341ec92b2b46045e6377
Caiyi Shyu <cai1hsu@outlook.com>
2025-06-29 17:56:07 +0800

    feat(constants): double the kernel heap size

--- crates/constants/src/lib.rs
+++ crates/constants/src/lib.rs
@@ -14,5 +14,5 @@ pub use errno::{ErrNo, SyscallError};
 pub const PROCESSOR_COUNT: usize = 2;
 
 pub const PAGE_SIZE: usize = 4096;
-pub const KERNEL_HEAP_SIZE: usize = 0x0100_0000;
+pub const KERNEL_HEAP_SIZE: usize = 0x0200_0000;
 pub const USER_STACK_SIZE: usize = 0x10_0000; // 1MB
