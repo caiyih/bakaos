d13840d5e7d56ddad85d6c778867cce4dc68fae6
Caiyi Shyu <cai1hsu@outlook.com>
2025-07-28 18:12:55 +0800

    build(drivers): Use source generation for platform-specific code
    - Add build.rs script to generate platform-specific code
    - Introduce source-generation crate as a build dependency
    - Remove manual platform feature checks and code selection
    - Update lib.rs to use generated code
    - Add .gitignore to exclude generated source files

--- crates/drivers/src/lib.rs
+++ crates/drivers/src/lib.rs
@@ -10,39 +10,19 @@ pub use block::*;
 #[cfg(target_arch = "riscv64")]
 mod riscv64;
 
-#[allow(unused_imports)]
-#[cfg(target_arch = "riscv64")]
-use riscv64::*;
-
 #[cfg(target_arch = "loongarch64")]
 mod loongarch64;
-
-#[allow(unused_imports)]
-#[cfg(target_arch = "loongarch64")]
-use loongarch64::*;
 
 mod rtc;
 pub use rtc::{current_timespec, current_timeval, ITimer, UserTaskTimer};
 
+#[rustfmt::skip]
+mod generated;
+pub use generated::*;
+
 pub fn initialize_rtc() {
-    let rtc_offset = machine().get_rtc_offset();
+    platform_specific::legacy_println!("Initializing RTC...");
+    let machine = machine();
+    let rtc_offset = machine.get_rtc_offset();
     rtc::initialize(rtc_offset);
-}
-
-cfg_if::cfg_if! {
-    if #[cfg(target_arch = "riscv64")] {
-        type MachineImpl = riscv64::MachineImpl;
-    } else if #[cfg(target_arch = "loongarch64")] {
-        type MachineImpl = loongarch64::MachineImpl;
-    } else {
-        compile_error!("No valid machine feature enabled");
-    }
-}
-
-static MACHINE_IMPL: MachineImpl = MachineImpl::new();
-
-#[inline(always)]
-#[allow(unreachable_code)]
-pub fn machine() -> &'static dyn IMachine {
-    &MACHINE_IMPL
 }
