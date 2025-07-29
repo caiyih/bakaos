a5d9ff4740a5aaec622d2b4bdefcf8a71869faf7
Caiyi Shyu <cai1hsu@outlook.com>
2025-07-30 01:46:00 +0800

    refactor(loongarch64): comment out RTC initialization code
    - Disable RTC initialization code by commenting it out
    - Add FIXME comment suggesting to move this code to a separate module
    - Note that the code is specific to QEMU

--- crates/platform-abstractions/src/loongarch64/boot.rs
+++ crates/platform-abstractions/src/loongarch64/boot.rs
@@ -149,19 +149,22 @@ extern "C" fn main_processor_init(r21: usize) {
         }
     }
 
-    // loongson,ls7a-rtc
-    // https://github.com/qemu/qemu/blob/661c2e1ab29cd9c4d268ae3f44712e8d421c0e56/include/hw/pci-host/ls7a.h#L45
-    const RTC_BASE: usize = 0x10000000 + 0x00080000 + 0x00050100;
-    const SYS_RTCCTRL: usize = 0x40;
+    // FIXME: move this to a separate module
+    // The code only applies to QEMU
+    //
+    // // loongson,ls7a-rtc
+    // // https://github.com/qemu/qemu/blob/661c2e1ab29cd9c4d268ae3f44712e8d421c0e56/include/hw/pci-host/ls7a.h#L45
+    // const RTC_BASE: usize = 0x10000000 + 0x00080000 + 0x00050100;
+    // const SYS_RTCCTRL: usize = 0x40;
 
-    const RTC_MASK: u64 = ((!0u64) >> (64 - (1))) << (13);
-    const TOY_MASK: u64 = ((!0u64) >> (64 - (1))) << (11);
-    const EO_MASK: u64 = ((!0u64) >> (64 - (1))) << (8);
+    // const RTC_MASK: u64 = ((!0u64) >> (64 - (1))) << (13);
+    // const TOY_MASK: u64 = ((!0u64) >> (64 - (1))) << (11);
+    // const EO_MASK: u64 = ((!0u64) >> (64 - (1))) << (8);
 
-    let rtc_ctrl = ((RTC_BASE + SYS_RTCCTRL) | 0x8000_0000_0000_0000) as *mut u32;
-    unsafe {
-        rtc_ctrl.write_volatile((TOY_MASK | EO_MASK | RTC_MASK) as u32);
-    }
+    // let rtc_ctrl = ((RTC_BASE + SYS_RTCCTRL) | 0x8000_0000_0000_0000) as *mut u32;
+    // unsafe {
+    //     rtc_ctrl.write_volatile((TOY_MASK | EO_MASK | RTC_MASK) as u32);
+    // }
 
     unsafe { clear_bss() };
 
