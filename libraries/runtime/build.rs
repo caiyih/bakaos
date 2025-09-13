cef71f1ad55a6d7607dc8ccee846468d7993d545
Caiyi Shyu <cai1hsu@outlook.com>
2025-09-14 01:50:58 +0800

    feat(runtime): add serde_json as a build dependency and improve std feature checks

--- libraries/runtime/build.rs
+++ libraries/runtime/build.rs
@@ -1,3 +1,5 @@
+use serde_json::Value;
+
 pub fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-env-changed={}", feature_env("std"));
@@ -6,7 +8,7 @@ pub fn main() {
     // see https://doc.rust-lang.org/nightly/rustc/check-cfg/cargo-specifics.html#cargorustc-check-cfg-for-buildrsbuild-script
     println!("cargo:rustc-check-cfg=cfg(runtime_std)");
 
-    if check_env(&test_env()) || check_env(&feature_env("std")) {
+    if (is_target_support_std() || check_env(&test_env())) && !check_env(&feature_env("no_std")) {
         println!("cargo:rustc-cfg=runtime_std");
     }
 }
@@ -21,4 +23,30 @@ fn test_env() -> String {
 
 fn check_env(var: &str) -> bool {
     std::env::var(var).is_ok()
+}
+
+fn get_target() -> String {
+    std::env::var("TARGET").unwrap_or_else(|_| String::from("unknown"))
+}
+
+fn is_target_support_std() -> bool {
+    let target = get_target();
+
+    let output = std::process::Command::new("rustc")
+        .args(&[
+            "--print",
+            "target-spec-json",
+            "--target",
+            &target,
+            "-Z",
+            "unstable-options",
+        ])
+        .output()
+        .expect("failed to execute process");
+
+    let output = String::from_utf8_lossy(&output.stdout);
+
+    let json: Value = serde_json::from_str(&output).unwrap();
+
+    json["metadata"]["std"].as_bool().unwrap()
 }
