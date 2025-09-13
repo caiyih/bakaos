f38fe91991e60fc2f9bb9b8255764bf44e4b4f50
Caiyi Shyu <cai1hsu@outlook.com>
2025-09-13 19:37:00 +0800

    build(runtime): implement conditional compilation for runtime_std
    - Add build.rs script to manage conditional compilation
    - Introduce runtime_std cfg flag based on environment variables
    - Update lib.rs to use runtime_std instead of feature gates
    - Adjust module visibility and usage based on runtime_std

--- libraries/runtime/src/lib.rs
+++ libraries/runtime/src/lib.rs
@@ -1,26 +1,27 @@
 #![cfg_attr(
-    not(feature = "std"),
+    not(runtime_std),
     no_std,
     allow(internal_features),
-    feature(core_intrinsics)
+    feature(core_intrinsics, linkage)
 )]
 
-mod baremetal;
+#[cfg(all(not(runtime_std), target_arch = "riscv64"))]
+pub mod baremetal;
 mod hosted;
 
-#[cfg(not(feature = "std"))]
-#[allow(unused_imports)] // macros from `alloc` are not used on all platforms
 #[macro_use]
+#[allow(unused_imports)] // macros from `alloc` are not used on all platforms
+#[cfg(not(runtime_std))]
 extern crate alloc as alloc_crate;
 
-#[cfg(not(feature = "std"))]
+#[cfg(not(runtime_std))]
 mod std_compat;
 
-#[cfg(not(feature = "std"))]
+#[cfg(not(runtime_std))]
 pub use std_compat::*;
 
 // Use std directly when available
-#[cfg(feature = "std")]
+#[cfg(runtime_std)]
 pub use ::std::*;
 
 #[cfg(feature = "boot")]
